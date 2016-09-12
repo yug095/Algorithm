83a653fb1f54777c452ee8c0c14e642653a4bdcb
yug095 <yug095@eng.ucsd.edu>
2016-09-11 22:52:17 -0700

    push two sum

--- sol333.java
+++ sol333.java
@@ -38,3 +38,21 @@ public class Solution {
     }
     
 }
+
+
+// 1. Two Sum 
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int [] res = new int [2];
+        int len = nums.length;
+        for (int i = 0; i< len -1 ;i++) {
+            for (int j = i + 1; j<len; j ++) {
+                if (nums[i] + nums[j] == target) {
+                    res[0] = i; 
+                    res[1] = j;
+                }
+            }
+        }
+        return res;
+    }
+}
